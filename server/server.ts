3266b4e68cf612480a80f315f013b5926b93e8dc
dominikdeimel <info@dominikdeimel.com>
2022-08-06 20:08:53 +0200

    chore(server): added error handling function for db calls

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv'
 import { PrismaClient } from '@prisma/client'
 import fastifyCors from '@fastify/cors'
 import fastifySensible from '@fastify/sensible'
+import { HttpError } from '@fastify/sensible/lib/httpError'
 
 // Dotenv Config
 config()
@@ -14,13 +15,19 @@ app.register(fastifyCors)
 app.register(fastifySensible)
 
 app.get('/posts', async (req, res) => {
-  return await prisma.post.findMany({
+  return await commitToDB(prisma.post.findMany({
     select: {
       id: true,
       title: true
     }
-  })
+  }))
 })
+
+async function commitToDB<T> (promise: Promise<T>): Promise<T | HttpError> {
+  const [error, data] = await app.to(promise)
+  if (error) return app.httpErrors.internalServerError(error.message)
+  return data
+}
 
 app.listen({
   port: Number(process.env.PORT) || 3000,
